6b0200d16536212e0d380d06eb17e49a86a7cae6
Felix Warner (RIT Student) <fgw7468@rit.edu>
2022-11-28 13:37:09 -0500

    Circle test

--- server/test/utils/index.js
+++ server/test/utils/index.js
@@ -1,5 +1,34 @@
-// const config = require('../test_config.js');
-// const utils = require('../../src/utils');
-//
-// const { limits } = config;
-//
+const config = require('../test_config.js');
+const utils = require('../../src/utils');
+
+const { limits } = config;
+
+describe('utils', () => {
+  describe('add', () => {
+    it('should return the addition of two numbers', (done) => {
+      const sum = utils.add(5, 10);
+      sum.should.equal(15);
+      done();
+    });
+
+    it('should clamp the numbers to the minimum', (done) => {
+      const sum = utils.add(limits.MIN - 100, 1);
+      sum.should.equal(limits.MIN);
+      done();
+    });
+
+    it('should clamp the numbers to the maximum', (done) => {
+      const sum = utils.add(limits.MAX, 1);
+      sum.should.equal(limits.MAX);
+      done();
+    });
+  });
+
+  describe('subtract', () => {
+    it('should return the difference of two numbers', (done) => {
+      const diff = utils.sub(10, 5);
+      diff.should.equal(5);
+      done();
+    });
+  });
+});
